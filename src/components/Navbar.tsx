5a84f50108b3d8308996ce7ea1d8fad9bb952624
Sadykov Arsen <LoCo10291998@gmail.com>
2025-02-14 18:32:42 +0600

    Commit init. Created by NavBar. And followed the links via Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,55 @@
+import { Link } from "react-router-dom";
+
+
 const Navbar = () => {
     return (
-        <div>
-
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/pages/home">
+                    My App
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/pages/home">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/pages/about">
+                                About
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/pages/contacts">
+                                Contacts
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/pages/services">
+                                Services
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/pages/blog">
+                                Blog
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/pages/admin">
+                                Admin
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 };
 
